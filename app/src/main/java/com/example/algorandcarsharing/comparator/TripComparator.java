77780f7505378eb8e1045398f03ed845c3627da5
Matteo Baratella <matteo.baratella96@gmail.com>
2022-05-03 11:06:56 +0200

    Revert "Sort fix"
    This reverts commit a99bc4c1d7f419b8953ef881ce7cb1d014173b0b.

--- app/src/main/java/com/example/algorandcarsharing/comparator/TripComparator.java
+++ app/src/main/java/com/example/algorandcarsharing/comparator/TripComparator.java
@@ -7,10 +7,6 @@ import java.util.Comparator;
 public class TripComparator implements Comparator<TripModel> {
     @Override
     public int compare(TripModel o1, TripModel o2) {
-        // order by id desc
-        if(o1.id() <= o2.id())
-            return 1;
-        else
-            return 0;
+        return o1.id().compareTo(o2.id());
     }
 }
